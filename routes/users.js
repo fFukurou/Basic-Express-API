7a959e0d259741241910600b6edb9f9c1c1bcac3
fFukurou <supevi1@gmail.com>
2025-03-27 06:07:06 -0300

    cooler routing, middleware, rendering static files, parsing form...

--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,55 @@
 const express = require('express');
 const router = express.Router();
 
-
+router.use(logger);
 
 router.get('/', (req, res) => {
+    console.log( req.query.name );
     res.send("User List");
 })
 
 
 router.get('/new', (req, res) => {
-    res.send("User New Form");
+    res.render("users/new", { firstName: "Test" });
 })
+
+
+
+router.post('/', (req, res) => {
+    const isValid = true;
+    if (isValid) {
+        users.push( {firstName: req.body.firstName });
+        res.redirect(`/users/${users.length - 1}`);
+    } else {
+        console.log("Error");
+        res.render('users/new', { firstName: req.body.firstName });
+    }
+})
+
+
+router.route("/:id").get((req, res) => {
+    console.log(req.user);
+    // Inside 'new.ejs', we set the name="firstName" in the input, so that's what we're using here.
+    res.send(`Hi ${req.user['name']}`);
+}).put((req, res) => {
+    res.send(`Update User With ID ${req.params.id}`);
+}).delete((req, res) => {
+    res.send(`Delete User With ID ${req.params.id}`);
+})
+
+
+const users = [{ name: 'fFukurou' }, { name: 'Psylocke'}];
+// MIDDLEWARE
+router.param("id", (req, res, next, id) => {
+    req.user = users[id];
+    next();
+})
+
+
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,24 +1,32 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded( { extended: true }));
+//  allows us to parse json from the body
+app.use(express.json());
+
 app.set('view engine', 'ejs');
 
+// WE want this middleware to be used on every single request, so define it at the top
+app.use(logger);
 
 
-app.get('/', (req, res) => {
-     console.log("here");
-    //  res.json({ message: "Error "});
-    res.render('index', { text123: "World" });
-}) 
+// app.get('/', logger ,(req, res) => {
+//     //  console.log("here");
+//     //  res.json({ message: "Error "});
+//     res.render('index', { text123: "World" });
+// }) 
 
 
 const userRouter = require('./routes/users');
 
 app.use('/users', userRouter);
 
-
-
-
-
+// MIDDLEWARE
+function logger(req, res, next) {
+    console.log(req.originalUrl);
+    next();
+}
 
 app.listen(3000);
